a01277c09f2cec61294f08f66c156abf09b2d1f8
MadCom96 <70102600+MadCom96@users.noreply.github.com>
2022-06-07 02:57:57 +0900

    [helpers.date.dateHelper][getSortableDate] Made up a test unit class and added an unit test for get SortableDate method. Checked if the output is sortable.

---
+++ omniNotes/src/test/java/it/feio/android/omninotes/MyTest.java
@@ -0,0 +1,57 @@
+package it.feio.android.omninotes;
+
+import static org.junit.Assert.assertTrue;
+import static java.lang.Thread.sleep;
+import static it.feio.android.omninotes.utils.ConstantsBase.DATE_FORMAT_SORTABLE;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import it.feio.android.omninotes.helpers.date.DateHelper;
+
+public class MyTest {
+    DateHelper dateHelper;
+    @Before
+    public void setUp(){
+    }
+    @Test
+    public void getSortableDateTest() throws ParseException, InterruptedException {
+        class SDPair{
+            String s;
+            Date d;
+            SDPair(String s, Date d){
+                this.s = s;
+                this.d = d;
+            }
+            public Date getD() { return d; }
+            public String getS() { return s; }
+        }
+
+        List<SDPair> stringDatas = new LinkedList<> ();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_SORTABLE);
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++){
+            int time = random.nextInt(1000);
+            sleep(time);
+            String data = dateHelper.getSortableDate();
+            stringDatas.add(new SDPair(data, sdf.parse(data)));
+        }//이미 정렬되어 나올 것
+        //역순정렬
+        Collections.sort(stringDatas, (a, b) -> b.getS().compareTo(a.getS()));
+        for (int i = 1; i < 9; i++){
+            //역순으로 잘 정렬이 되었는가
+            assertTrue(stringDatas.get(i).getD().compareTo(stringDatas.get(i - 1).getD()) < 1);
+        }
+    }
+
+
+}
